d0168692713ae94fcfbf40c3b318b60199965f4f
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2025-09-29 10:08:24 +0200

    chore: add settings for cSpell and Structurizr Lite

---
+++ scripts/structurizr-lite.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# This script starts Structurizr Lite: https://structurizr.com/help/lite/getting-started
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker run -it --rm -p 8080:8080 -v ${SCRIPT_DIR}/../docs/c4:/usr/local/structurizr structurizr/lite
